9f1135705ba826b896f2af851e316eb25957735a
Diego Bittencourt Mendes <mendesdiego90@gmail.com>
2023-04-09 19:55:15 +0900

    the backend is still buggy

---
+++ api/routes/index.js
@@ -0,0 +1,5 @@
+var taskList = require('../controllers');
+
+module.exports = function(app) {
+
+}
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require("express")
-const app = express()
 
-app.get("/", (request, response) => {
-    response.send("Hello from the server");
+import express from 'express';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { generateResponse } from "./api/controllers/index.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.post("/generate", generateResponse);
+app.get("/", (req, res) => {
+    console.log('here too')
+    res.send('ok')
 })
 
-app.listen(3000, () => {
-    console.log("Listen to the port 300...")
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
